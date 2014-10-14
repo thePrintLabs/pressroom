89bc2eca5c6e6222ebf7984523336a7e6257a5be
j4ym <gianmarco.fiorello@gmail.com>
2014-10-14 16:21:02 +0200

    pr api prev,next

--- core/api.php
+++ core/api.php
@@ -71,3 +71,68 @@ function pr_get_option( $option ) {
 
    return false;
 }
+
+/**
+ * get edition link with book protocol
+ * @param  int $edition_id
+ * @return string
+ */
+function pr_book( $edition_id ) {
+
+  $edition = get_post( $edition_id );
+  $book_url = str_replace( array( 'http://', 'https://' ), 'book://', TPL_HPUB_URI );
+
+  return $book_url . TPL_Utils::sanitize_string( $edition->post_title . '.hpub' );
+}
+
+/**
+ * get previous or next post in edition posts array
+ * @param  int $post_id
+ * @param  int $edition_id
+ * @param  boolean $prev
+ * @param  boolean $next
+ * @return string or boolean
+ */
+function pr_get_edition_post( $post_id, $edition_id, $position = '' ) {
+
+  $linked_posts = pr_get_edition_posts( $edition_id, true );
+  $linked_posts = $linked_posts->posts;
+  foreach( $linked_posts as $k => $post ) {
+
+    if( $post->ID == $post_id ) {
+      if( $position == 'prev' && $k > 0 ) {
+        $previous = ( isset( $linked_posts[$k-1] ) ? $linked_posts[$k-1] : false );
+        return $previous->guid;
+      }
+      else if ( $position == 'next' ) {
+        $nextpost = ( isset( $linked_posts[$k+1] ) ? $linked_posts[$k+1] : false );
+        if( $nextpost )
+          return $nextpost->guid;
+      }
+      else
+        return;
+    }
+  }
+}
+
+/**
+ * get previous post
+ * @param  int $current_post_id
+ * @param  int $edition_id
+ * @return string or boolean
+ */
+function pr_prev( $current_post_id, $edition_id ) {
+
+  return pr_get_edition_post( $current_post_id, $edition_id, 'prev' );
+}
+
+/**
+ * get next post
+ * @param  int $current_post_id
+ * @param  int $edition_id
+ * @return string or boolean
+ */
+function pr_next( $current_post_id, $edition_id ) {
+
+  return pr_get_edition_post( $current_post_id, $edition_id, 'next' );
+}

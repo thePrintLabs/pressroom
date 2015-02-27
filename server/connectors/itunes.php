c97ac11dbe45790ac326a1f8e841d8f3a255d566
j4ym <gianmarco.fiorello@gmail.com>
2015-02-27 19:43:51 +0100

    itunes connector revision

--- server/connectors/itunes.php
+++ server/connectors/itunes.php
@@ -60,6 +60,7 @@ final class PR_Connector_iTunes extends PR_Server_API {
 
   /**
    * Get iTunes shared secret
+   *
    * @return string
    */
   public function get_shared_secret() {
@@ -69,71 +70,42 @@ final class PR_Connector_iTunes extends PR_Server_API {
 
   /**
    * Save base64-encoded receipt from App Store to db.
-   * @param string or boolean false $transaction_id
-   * @return int
+   *
+   * @param object $receipt
+   * @param string $transaction_id
+   * @param string $purchase_type
+   * @return boolean
    */
-  public function save_receipt( $transaction_id ) {
+  public function save_receipt( $receipt, $transaction_id, $purchase_type ) {
 
     global $wpdb;
-    // Old receipt validation method
-    if ( $transaction_id ) {
-      $sql = "SELECT receipt_id FROM " . $wpdb->prefix . PR_TABLE_RECEIPTS . " ";
-      $sql.= "WHERE app_bundle_id = %s AND device_id = %s AND transaction_id = %s";
-      $receipt_id = $wpdb->get_var( $wpdb->prepare( $sql, $this->app_id, $this->user_id, $transaction_id ) );
-      if ( $receipt_id ) {
-        $sql = "UPDATE " . $wpdb->prefix . PR_TABLE_RECEIPTS . " SET base64_receipt = %s WHERE receipt_id = %d";
-        $wpdb->query( $wpdb->prepare( $sql, $this->base64_receipt, $receipt_id ) );
-      }
-      else {
-        $wpdb->insert(
-          $wpdb->prefix . PR_TABLE_RECEIPTS,
-          array(
-             'app_bundle_id'  => $this->app_id,
-             'device_id'      => $this->user_id,
-             'transaction_id' => $transaction_id,
-             'base64_receipt' => $this->base64_receipt
-          ), array( '%s', '%s', '%s', '%s' )
-        );
-        $receipt_id = $wpdb->insert_id;
-      }
+
+    $sql = "SELECT receipt_id FROM " . $wpdb->prefix . PR_TABLE_RECEIPTS . " ";
+    $sql.= "WHERE app_bundle_id = %s AND transaction_id = %s";
+    $receipt_id = $wpdb->get_var( $wpdb->prepare( $sql, $this->app_id, $transaction_id ) );
+
+    if ( !$receipt_id ) {
+
+      $sql = "INSERT IGNORE INTO " . $wpdb->prefix . PR_TABLE_RECEIPTS . " SET ";
+      $sql.= "app_bundle_id = %s, device_id = %s, base64_receipt = %s, transaction_id = %s, product_id = %s, type = %s";
+
+      $out = $wpdb->query( $wpdb->prepare(
+        $sql,
+        $this->app_id,
+        $this->user_id,
+        $this->base64_receipt,
+        $transaction_id,
+        $receipt->product_id,
+        $purchase_type
+      ) );
+
+      return $out;
     }
     else {
-      $sql = "SELECT receipt_id FROM " . $wpdb->prefix . PR_TABLE_RECEIPTS . " ";
-      $sql.= "WHERE app_bundle_id = %s AND device_id = %s";
-      $receipt_id = $wpdb->get_var( $wpdb->prepare( $sql, $this->app_id, $this->user_id ) );
-      if ( $receipt_id ) {
-        $sql = "UPDATE " . $wpdb->prefix . PR_TABLE_RECEIPTS . " SET base64_receipt = %s WHERE receipt_id = %d";
-        $wpdb->query( $wpdb->prepare( $sql, $this->base64_receipt, $receipt_id ) );
-      }
-      else {
-        $wpdb->insert(
-          $wpdb->prefix . PR_TABLE_RECEIPTS,
-          array(
-             'app_bundle_id'  => $this->app_id,
-             'device_id'      => $this->user_id,
-             'base64_receipt' => $this->base64_receipt
-          ), array( '%s', '%s', '%s' )
-        );
-        $receipt_id = $wpdb->insert_id;
-      }
+      return true;
     }
-    return $receipt_id;
   }
 
-  /**
-   * Save receipt transactions into the db.
-   * @param int $receipt_unique_id
-   * @param object $receipt
-   * @param string $purchase_type
-   * @return mixed
-   */
-  public function save_receipt_transactions( $receipt_unique_id, $receipt, $purchase_type ) {
-
-    global $wpdb;
-    $sql = "INSERT IGNORE INTO " . $wpdb->prefix . PR_TABLE_RECEIPT_TRANSACTIONS . " SET ";
-    $sql.= "receipt_id = %d, transaction_id = %s, product_id = %s, type = %s";
-    return $wpdb->query( $wpdb->prepare( $sql, $receipt_unique_id, $receipt->transaction_id, $receipt->product_id, $purchase_type ) );
-  }
 
   /*
    * Verify a base64-encoded receipt with the App Store.
@@ -206,10 +178,9 @@ final class PR_Connector_iTunes extends PR_Server_API {
   public function get_latest_subscription_receipt() {
 
     global $wpdb;
-    $sql = "SELECT DISTINCT r.base64_receipt, t.transaction_id FROM " . $wpdb->prefix . PR_TABLE_RECEIPTS . " r ";
-    $sql.= "LEFT JOIN " . $wpdb->prefix . PR_TABLE_RECEIPT_TRANSACTIONS . " t ON r.receipt_id = t.receipt_id ";
-    $sql.= "WHERE r.app_bundle_id = %s AND r.device_id = %s AND t.type = 'auto-renewable-subscription' ";
-    $sql.= "ORDER BY t.transaction_id DESC LIMIT 0, 1";
+    $sql = "SELECT DISTINCT base64_receipt, transaction_id FROM " . $wpdb->prefix . PR_TABLE_RECEIPTS;
+    $sql.= " WHERE app_bundle_id = %s AND device_id = %s AND type = 'auto-renewable-subscription'";
+    $sql.= " ORDER BY transaction_id DESC LIMIT 0, 1";
     $data = $wpdb->get_row( $wpdb->prepare( $sql, $this->app_id, $this->user_id ) );
     return $data;
 
@@ -224,10 +195,9 @@ final class PR_Connector_iTunes extends PR_Server_API {
   public function get_purchased_editions() {
 
     global $wpdb;
-    $sql = "SELECT DISTINCT t.product_id FROM " . $wpdb->prefix . PR_TABLE_RECEIPTS . " r ";
-    $sql.= "LEFT JOIN " . $wpdb->prefix . PR_TABLE_RECEIPT_TRANSACTIONS . " t ON r.receipt_id = t.receipt_id ";
-    $sql.= "WHERE r.app_bundle_id = %s AND r.device_id = %s AND t.type = 'issue' ";
-    $sql.= "ORDER BY t.transaction_id DESC";
+    $sql = "SELECT DISTINCT product_id FROM " . $wpdb->prefix . PR_TABLE_RECEIPTS ;
+    $sql.= " WHERE app_bundle_id = %s AND device_id = %s AND type = 'issue'";
+    $sql.= " ORDER BY transaction_id DESC";
     $data = $wpdb->get_results( $wpdb->prepare( $sql, $this->app_id, $this->user_id ) );
     return $data;
   }
@@ -235,42 +205,21 @@ final class PR_Connector_iTunes extends PR_Server_API {
   /**
    *
    * Get multiple purchased editions
+   *
    * @param object $receipt_data
-   * @param object $receipt
    * @return array
    */
-  public function get_editions_in_subscription( $receipt_data, $receipt ) {
+  public function get_editions_in_subscription( $receipt_data ) {
 
     global $wpdb;
     $issues = array();
-    $transaction_id = $receipt->transaction_id;
-    $from_date = date( 'Y-m-d H:i', (int)$receipt->original_purchase_date_ms / 1000 );
+    $transaction_id = $receipt_data->receipt->transaction_id;
+
     if ( $receipt_data->status == 0 ) {
-      if ( is_array( $receipt_data->latest_receipt_info ) ) {
-        foreach ( $receipt_data->latest_receipt_info as $receipt_info ) {
-          if ( $receipt_info->transaction_id == $transaction_id ) {
-            $to_date = date( 'Y-m-d H:i', (int)( ( isset( $receipt_info->expires_date_ms ) ? $receipt_info->expires_date_ms : $receipt_info->expires_date ) / 1000 ) );
-            break;
-          }
-        }
-      }
-      else {
-        $to_date = date( 'Y-m-d H:i', (int)( ( isset( $receipt_data->latest_receipt_info->expires_date_ms ) ? $receipt_data->latest_receipt_info->expires_date_ms : $receipt_data->latest_receipt_info->expires_date ) / 1000 ) );
-      }
+      $to_date = date( 'Y-m-d H:i', (int)( ( isset( $receipt_data->latest_receipt_info->expires_date_ms ) ? $receipt_data->latest_receipt_info->expires_date_ms : $receipt_data->latest_receipt_info->expires_date ) / 1000 ) );
     }
     elseif ( $receipt_data->status == 21006 ) {
-      if ( is_array( $receipt_data->latest_expired_receipt_info ) ) {
-        foreach ( $receipt_data->latest_expired_receipt_info as $expired_receipt_info ) {
-
-          if ( $expired_receipt_info->transaction_id == $transaction_id ) {
-            $to_date = date( 'Y-m-d H:i', (int)( ( isset( $expired_receipt_info->expires_date_ms ) ? $expired_receipt_info->expires_date_ms : $expired_receipt_info->expires_date ) / 1000 ) );
-            break;
-          }
-        }
-      }
-      else {
-        $to_date = date( 'Y-m-d H:i', (int)( ( isset( $receipt_data->latest_expired_receipt_info->expires_date_ms ) ? $receipt_data->latest_expired_receipt_info->expires_date_ms : $receipt_data->latest_expired_receipt_info->expires_date ) / 1000 ) );
-      }
+      $to_date = date( 'Y-m-d H:i', (int)( ( isset( $receipt_data->latest_expired_receipt_info->expires_date_ms ) ? $receipt_data->latest_expired_receipt_info->expires_date_ms : $receipt_data->latest_expired_receipt_info->expires_date ) / 1000 ) );
     }
 
     // Get the editorial project settings
@@ -283,21 +232,21 @@ final class PR_Connector_iTunes extends PR_Server_API {
     }
 
     $today = date('Y-m-d H:i');
-    $subscription_method = PR_Editorial_Project::get_subscription_method( $this->eproject->term_id, $receipt->product_id );
+    $subscription_method = PR_Editorial_Project::get_subscription_method( $this->eproject->term_id, $receipt_data->receipt->product_id );
 
     // Enable all editions if the subscription method is not defined or is setted on all
     // and the current date is between the subscription range
-    if ( ( !$subscription_method || $subscription_method == 'all' ) && ( $from_date <= $today && $to_date >= $today ) ) {
+    if ( ( !$subscription_method || $subscription_method == 'all' ) && ( $to_date >= $today ) ) {
       $editions = PR_Editorial_Project::get_all_editions( $this->eproject );
     }
     elseif ( $subscription_method == 'last' ) {
       // Set the start date of subscription equal to latest published edition date,
       // if the expiring date is greater or equal to current date and the start date of subscription
       // is greater than the last published edition date
-      if ( $to_date >= $today && $from_date > $last_edition_date ) {
-        $from_date = $last_edition_date;
+      if ( $to_date >= $today && $today > $last_edition_date ) {
+        $today = $last_edition_date;
       }
-      $editions = PR_Editorial_Project::get_editions_in_range( $this->eproject, $from_date, $to_date );
+      $editions = PR_Editorial_Project::get_editions_in_range( $this->eproject, $today, $to_date );
     }
 
     if ( !empty( $editions ) ) {
@@ -311,6 +260,7 @@ final class PR_Connector_iTunes extends PR_Server_API {
 
   /**
    * Get all purchased editions
+   *
    * @return array
    */
   public function get_purchases() {
@@ -332,22 +282,9 @@ final class PR_Connector_iTunes extends PR_Server_API {
       $receipt_data = $this->validate_receipt();
       if ( !isset( $receipt_data->error ) ) {
         $is_subscribed = $receipt_data->status == 0;
-        $receipt = $receipt_data->receipt;
 
-        // Support the new receipt validation method implemented from iOS7 version
-        if ( isset( $receipt->in_app ) ) {
-          $receipt_unique_id = $this->save_receipt( false );
-          foreach ( $receipt->in_app as $k => $single_receipt ) {
-
-            if ( $single_receipt->transaction_id == $transaction_id ) {
-              $editions = $this->get_editions_in_subscription( $receipt_data, $single_receipt );
-              $purchases = array_merge( $purchases, $editions );
-            }
-          }
-        }
-        else {
-          $receipt_unique_id = $this->save_receipt( $receipt->transaction_id );
-          $editions = $this->get_editions_in_subscription( $receipt_data, $receipt );
+        if ( $this->save_receipt( $receipt_data->receipt, $transaction_id, $purchase_type ) ) {
+          $editions = $this->get_editions_in_subscription( $receipt_data );
           $purchases = array_merge( $purchases, $editions );
         }
       }
@@ -361,6 +298,7 @@ final class PR_Connector_iTunes extends PR_Server_API {
 
   /**
    * Check purchased editions on issue download
+   *
    * @param  boolean $allow_download
    * @param  string $app_id
    * @param  string $user_id
@@ -389,6 +327,7 @@ final class PR_Connector_iTunes extends PR_Server_API {
   /**
    * After each In-App Purchase, Client will call this API endpoint
    * to send the transaction receipt back to the server.
+   *
    * @return json string
    */
   protected function _action_purchase_confirmation() {
@@ -423,31 +362,28 @@ final class PR_Connector_iTunes extends PR_Server_API {
     }
 
     $receipt = $receipt_data->receipt;
-    // Support the new receipt validation method implemented from iOS7 version
-    if ( isset( $receipt->in_app ) ) {
-      $receipt_unique_id = $this->save_receipt( false );
-      foreach ( $receipt->in_app as $k => $single_receipt ) {
-
-        if ( $product_id && $product_id == $single_receipt->product_id ) {
-          $this->save_receipt_transactions( $receipt_unique_id, $single_receipt, $purchase_type );
-          // Record purchase
-          PR_Stats::increment_counter( 'purchase_' . $purchase_type );
-        }
+    // if transaction id == original transaction id is a new purchase else it's a restore
+    if ( $receipt->transaction_id == $receipt->original_transaction_id ) {
+      if ( $this->save_receipt( $receipt, $receipt->transaction_id, $purchase_type ) ) {
+        PR_Stats::increment_counter( 'purchase_' . $purchase_type );
+        $this->send_response( 200 );
       }
     }
     else {
-      $receipt_unique_id = $this->save_receipt( $receipt->transaction_id );
-      $this->save_receipt_transactions( $receipt_unique_id, $receipt, $purchase_type );
-      // Record purchase
-      PR_Stats::increment_counter( 'purchase_' . $purchase_type );
+
+      if ( $this->save_receipt( $receipt, $receipt->original_transaction_id, $purchase_type ) ) {
+        PR_Stats::increment_counter( 'purchase_' . $purchase_type );
+        $this->send_response( 200 );
+      }
     }
 
-    $this->send_response( 200 );
+    $this->send_response( 500 );
   }
 
   /**
    * Client will call this API endpoint to get a list of purchased issues and whether the user has an active subscription or not.
    * Baker will unlock for download all purchased issues.
+   *
    * @return json string
    */
   protected function _action_purchases_list() {
